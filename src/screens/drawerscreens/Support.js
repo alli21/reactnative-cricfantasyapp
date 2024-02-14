60fbde10add652d6b6219d6c3dd94cefd2c24af0
Aain Ur Rehman <aain134@gmail.com>
2024-02-14 16:20:44 +0500

    add setting and supports files

--- src/screens/drawerscreens/Support.js
+++ src/screens/drawerscreens/Support.js
@@ -1,30 +1,49 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, StatusBar, Linking } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import React, { useState } from 'react';
+import { Animated, View, Text, StatusBar, TouchableOpacity, StyleSheet, LayoutAnimation, Platform, UIManager, FlatList } from 'react-native';
+import { colors, constants, scale, scaleFont, verticalScale } from '../../utils';
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { scaleFont, verticalScale, scale, colors, constants } from "../../utils";
-import { useSelector } from "react-redux";
-import LinearGradient from "react-native-linear-gradient";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { useSelector } from 'react-redux';
+import LinearGradient from 'react-native-linear-gradient';
 
 
-const Support = (props) => {
+const Settings = (props) => {
 
     const theme = useSelector(state => state.theme)
-    const [number, setnumber] = useState(9876543210);
-    const [email, setemail] = useState(`mailto:${"markjhonson@email.com"}`);
 
+    const [expanded1, setexpanded1] = useState(false)
+    const [expanded2, setexpanded2] = useState(false)
+    const [expanded3, setexpanded3] = useState(false)
+    const [lang, setlang] = useState("English (USA)")
+    const [download, setdownload] = useState("Show Details")
+    const [quality, setquality] = useState("YES")
+    const [language, setlanguage] = useState([
+        "Hindi", "English (USA)"
+    ])
+    const [downloads, setdownloads] = useState([
+        "Show Details", "Hide My Details"
+    ])
+    const [downloadquality, setdownloadquality] = useState([
+        "YES", "NO"
+    ])
 
-    const onPressMobileNumberClick = () => {
+    if (Platform.OS === 'android') {
+        UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
 
-        let phoneNumber = '';
-        if (Platform.OS === 'android') {
-            phoneNumber = `tel:${number}`;
-        } else {
-            phoneNumber = `telprompt:${number}`;
-        }
+    const toggleExpand1 = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        setexpanded1(!expanded1)
+    }
 
-        Linking.openURL(phoneNumber);
+    const toggleExpand2 = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        setexpanded2(!expanded2)
+    }
+
+    const toggleExpand3 = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        setexpanded3(!expanded3)
     }
 
 
@@ -33,10 +52,13 @@ const Support = (props) => {
             <StatusBar barStyle={"light-content"} backgroundColor={'transparent'} hidden={false} translucent={true}
             />
 
-            <LinearGradient
-                start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }}
-                colors={[colors.secondary_blue, colors.primary_blue]}
-                style={{ width: scale(365), height: verticalScale(80), alignSelf: 'center', }}>
+            <View
+                style={{
+                    width: scale(365),
+                    height: verticalScale(80),
+                    alignSelf: 'center',
+                    backgroundColor:colors.green
+                }}>
                 <View style={{ alignSelf: 'center', width: scale(365), marginTop: verticalScale(40), flexDirection: 'row', alignItems: 'center', }}>
                     <TouchableOpacity style={{ width: scale(150), paddingHorizontal: scale(20), }} onPress={() => { props.navigation.goBack() }} >
                         <MaterialCommunityIcons
@@ -45,73 +67,100 @@ const Support = (props) => {
                             color={colors.white}
                         />
                     </TouchableOpacity>
-                    <Text style={{ fontFamily: constants.OPENSANS_FONT_SEMI_BOLD, fontSize: scaleFont(18), color: colors.white }}>Support</Text>
+                    <Text style={{ fontFamily: constants.OPENSANS_FONT_SEMI_BOLD, fontSize: scaleFont(18), color: colors.white }}>Settings</Text>
                 </View>
-            </LinearGradient>
+            </View>
 
-            <View style={{ marginTop: verticalScale(30), justifyContent: 'center', width: scale(320), alignSelf: 'center' }}>
 
-                <View style={{ alignSelf: 'center' }}>
-                    <FontAwesome5
-                        name="trophy"
-                        size={verticalScale(120)}
-                        color={colors.primary_blue}
-                    />
-                </View>
+            <View style={{ width: scale(340), alignSelf: 'center', backgroundColor: theme.colors.background_primary, paddingVertical: verticalScale(20), paddingHorizontal: scale(10), borderRadius: verticalScale(6) }}>
 
-                <Text style={{ fontSize: scaleFont(18), marginTop: verticalScale(20), marginBottom: verticalScale(10), textAlign: 'center', color: theme.colors.text_primary, fontFamily: constants.OPENSANS_FONT_SEMI_BOLD }}>Connect With Us</Text>
+                <Text style={{ fontSize: scaleFont(12), color: colors.green, fontFamily: constants.OPENSANS_FONT_BOLD }}>Preferred App Language</Text>
+                <TouchableOpacity activeOpacity={0.7} style={{ flexDirection: 'row', height: verticalScale(40), justifyContent: 'space-between', alignItems: 'center', backgroundColor: theme.colors.background_primary, paddingEnd: scale(20) }} onPress={() => toggleExpand1()}>
+                    <Text style={{ fontSize: scaleFont(14), color: theme.colors.text_primary, }}>{lang}</Text>
+                    <FontAwesome name={expanded1 ? 'angle-up' : 'angle-down'} size={verticalScale(24)} color={colors.secondary_blue} />
+                </TouchableOpacity>
 
-                <View style={{ flexDirection: 'row', width: scale(280), justifyContent: 'space-between', alignSelf: 'center', marginTop: verticalScale(10) }}>
-                    <TouchableOpacity
-                        onPress={() => onPressMobileNumberClick()}
-                        style={{ flexDirection: 'row', borderColor: colors.primary_blue, borderWidth: verticalScale(2), borderRadius: verticalScale(30), width: scale(120), height: verticalScale(40), alignItems: 'center', justifyContent: 'center' }}>
-                        <Icon
-                            name="call"
-                            size={verticalScale(16)}
-                            color={colors.primary_blue}
+                {
+                    expanded1 ? (<Animated.View style={{ backgroundColor: theme.colors.background_primary, height: verticalScale(70) }}>
+                        <FlatList
+                            showsVerticalScrollIndicator={false}
+                            data={language}
+                            renderItem={({ item }) => <TouchableOpacity activeOpacity={0.5} onPress={() => { setlang(item), toggleExpand1() }} style={{ height: verticalScale(35), flexDirection: 'row', }}>
+                                <Text style={{
+                                    fontSize: scaleFont(14),
+                                    color: theme.colors.text_primary,
+                                }}>{item}</Text>
+                            </TouchableOpacity>
+                            }
                         />
-                        <Text style={{ fontSize: scaleFont(16), color: colors.primary_blue, marginLeft: scale(10) }}>Call us</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={() => Linking.openURL(email)}
-                        style={{ flexDirection: 'row', borderColor: colors.primary_blue, borderWidth: verticalScale(2), borderRadius: verticalScale(30), width: scale(120), height: verticalScale(40), alignItems: 'center', justifyContent: 'center' }}>
-                        <Icon
-                            name="md-mail-outline"
-                            size={verticalScale(16)}
-                            color={colors.primary_blue}
-                        />
-                        <Text style={{ fontSize: scaleFont(16), color: colors.primary_blue, marginLeft: scale(10) }}>Mail us</Text>
-                    </TouchableOpacity>
-                </View>
+                    </Animated.View>) : null
 
-                <View style={{}}>
-                    <Text style={{ fontSize: scaleFont(18), marginBottom: verticalScale(5), marginTop: verticalScale(20), color: theme.colors.text_primary, fontFamily: constants.OPENSANS_FONT_SEMI_BOLD }}>Address</Text>
-                    <Text style={{ fontSize: scaleFont(14), color: theme.colors.text_primary, fontFamily: constants.OPENSANS_FONT_MEDIUM }}>
-                        ABC Building INC.
-                    </Text>
-                    <Text style={{ fontSize: scaleFont(14), color: theme.colors.text_primary, fontFamily: constants.OPENSANS_FONT_MEDIUM }}>
-                        123 Corportate Estate
-                    </Text>
-                    <Text style={{ fontSize: scaleFont(14), color: theme.colors.text_primary, fontFamily: constants.OPENSANS_FONT_MEDIUM }}>
-                        XYZ Road
-                    </Text>
-                    <Text style={{ fontSize: scaleFont(14), color: theme.colors.text_primary, fontFamily: constants.OPENSANS_FONT_MEDIUM }}>
-                        Tokyo - 121321
-                    </Text>
-                </View>
+                }
+                <View style={{ width: scale(340), height: verticalScale(0.5), marginBottom: verticalScale(10), backgroundColor: colors.greyColour, alignSelf: "center" }} />
+
+
+
+                <Text style={{ fontSize: scaleFont(12), color: colors.green, fontFamily: constants.OPENSANS_FONT_BOLD }}>Privacy Settings</Text>
+                <TouchableOpacity activeOpacity={0.7} style={{ flexDirection: 'row', height: verticalScale(40), justifyContent: 'space-between', alignItems: 'center', backgroundColor: theme.colors.background_primary, paddingEnd: scale(20) }} onPress={() => toggleExpand2()}>
+                    <Text style={{ fontSize: scaleFont(14), color: theme.colors.text_primary, }}>{download}</Text>
+                    <FontAwesome name={expanded2 ? 'angle-up' : 'angle-down'} size={verticalScale(24)} color={colors.secondary_blue} />
+                </TouchableOpacity>
+
+                {
+                    expanded2 ? (<Animated.View style={{ backgroundColor: theme.colors.background_primary, height: verticalScale(70) }}>
+                        <FlatList
+                            showsVerticalScrollIndicator={false}
+                            data={downloads}
+                            renderItem={({ item }) => <TouchableOpacity activeOpacity={0.5} onPress={() => { setdownload(item), toggleExpand2() }} style={{ height: verticalScale(35), flexDirection: 'row', }}>
+                                <Text style={{
+                                    fontSize: scaleFont(14),
+                                    color: theme.colors.text_primary,
+                                }}>{item}</Text>
+                            </TouchableOpacity>
+                            }
+                        />
+                    </Animated.View>) : null
+
+                }
+                <View style={{ width: scale(340), height: verticalScale(0.5), marginBottom: verticalScale(10), backgroundColor: colors.greyColour, alignSelf: "center" }} />
+
+
+
+                <Text style={{ fontSize: scaleFont(12), color: colors.green, fontFamily: constants.OPENSANS_FONT_BOLD }}>Make Me Discoverable</Text>
+                <TouchableOpacity activeOpacity={0.7} style={{ flexDirection: 'row', height: verticalScale(40), justifyContent: 'space-between', alignItems: 'center', backgroundColor: theme.colors.background_primary, paddingEnd: scale(20) }} onPress={() => toggleExpand3()}>
+                    <Text style={{ fontSize: scaleFont(14), color: theme.colors.text_primary, }}>{quality}</Text>
+                    <FontAwesome name={expanded3 ? 'angle-up' : 'angle-down'} size={verticalScale(24)} color={colors.secondary_blue} />
+                </TouchableOpacity>
+                {
+                    expanded3 ? (<Animated.View style={{ backgroundColor: theme.colors.background_primary, height: verticalScale(70) }}>
+                        <FlatList
+                            showsVerticalScrollIndicator={false}
+                            data={downloadquality}
+                            renderItem={({ item }) => <TouchableOpacity activeOpacity={0.5} onPress={() => { setquality(item), toggleExpand3() }} style={{ height: verticalScale(35), flexDirection: 'row', }}>
+                                <Text style={{
+                                    fontSize: scaleFont(14),
+                                    color: theme.colors.text_primary,
+                                }}>{item}</Text>
+                            </TouchableOpacity>
+                            }
+                        />
+                    </Animated.View>) : null
+
+                }
+
+                <View style={{ width: scale(340), height: verticalScale(0.5), marginBottom: verticalScale(10), backgroundColor: colors.greyColour, alignSelf: "center" }} />
+
 
             </View>
+
+
         </View>
-    )
+    );
 }
 
-export default Support
+export default Settings;
+
 const styles = StyleSheet.create({
-    image: {
-        width: verticalScale(100),
-        height: verticalScale(100)
-    }
-})
 
 
-
+});
